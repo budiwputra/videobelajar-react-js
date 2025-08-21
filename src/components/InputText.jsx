3ef430866bed2fc2865d607a9c6d253013e40f13
budiwputra@gmail.com <budiwputra@gmail.com>
2025-08-22 00:27:47 +0700

    fix

--- src/components/InputText.jsx
+++ src/components/InputText.jsx
@@ -1,7 +1,7 @@
 
 const InputText = ({className, ...props}) => {
     return (
-        <input pla className={`${className} border border-other-border w-full text-sm sm:text-base font-normal leading-[140%] tracking-[0.2px]
+        <input className={`${className} border border-other-border w-full text-sm sm:text-base font-normal leading-[140%] tracking-[0.2px]
         rounded-[10px] py-[10px] px-[8px]
         `} type="text" {...props} />
 
