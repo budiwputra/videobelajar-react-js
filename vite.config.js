b54e0626d4c0c40bc305e5b8c43dc7f36f822630
budiwputra@gmail.com <budiwputra@gmail.com>
2025-11-27 22:17:41 +0700

    vite proxy

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  // server : {
-  //   proxy : {
-  //     "/api" : {
-  //       target : "https://videobelajar-backend-production.up.railway.app",
-  //       changeOrigin : true,
-  //       secure : false,
-  //       rewrite : (path) =>  path.replace(/^\/api/, "")
-  //     }
-  //   }
-  // }
+  server : {
+    proxy : {
+      "/api" : {
+        target : "https://videobelajar-backend-production.up.railway.app",
+        changeOrigin : true,
+        secure : false,
+        rewrite : (path) =>  path.replace(/^\/api/, "")
+      }
+    }
+  }
 })
